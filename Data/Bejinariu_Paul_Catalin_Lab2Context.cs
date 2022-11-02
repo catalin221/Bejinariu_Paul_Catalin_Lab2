338e56b0b1cdacf7917ddfe420d6dda6758fe212
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2022-11-02 22:35:35 +0200

    Add project files.

---
+++ Data/Bejinariu_Paul_Catalin_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Bejinariu_Paul_Catalin_Lab2.Models;
+
+namespace Bejinariu_Paul_Catalin_Lab2.Data
+{
+    public class Bejinariu_Paul_Catalin_Lab2Context : DbContext
+    {
+        public Bejinariu_Paul_Catalin_Lab2Context (DbContextOptions<Bejinariu_Paul_Catalin_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bejinariu_Paul_Catalin_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Bejinariu_Paul_Catalin_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Bejinariu_Paul_Catalin_Lab2.Data;
+using Bejinariu_Paul_Catalin_Lab2.Models;
+
+namespace Bejinariu_Paul_Catalin_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Bejinariu_Paul_Catalin_Lab2.Data.Bejinariu_Paul_Catalin_Lab2Context _context;
+
+        public IndexModel(Bejinariu_Paul_Catalin_Lab2.Data.Bejinariu_Paul_Catalin_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Publisher != null)
+            {
+                Publisher = await _context.Publisher.ToListAsync();
+            }
+        }
+    }
+}
