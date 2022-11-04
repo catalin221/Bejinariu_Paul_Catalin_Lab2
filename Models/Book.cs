d50df89eb240e35d9db18708d47e9a6071fa070e
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2022-11-04 16:18:22 +0200

    BookCategories, Publisher, PublisherID nullable

--- Models/Book.cs
+++ Models/Book.cs
@@ -18,9 +18,9 @@ namespace Bejinariu_Paul_Catalin_Lab2.Models
         [DataType(DataType.Date)]
         public DateTime PublishingDate { get; set; }
 
-        public int PublisherID { get; set; }
-        public Publisher Publisher { get; set; } // navigation property
-        public ICollection<BookCategory> BookCategories { get; set; }
+        public int? PublisherID { get; set; }
+        public Publisher? Publisher { get; set; } // navigation property
+        public ICollection<BookCategory>? BookCategories { get; set; }
 
     }
 }
