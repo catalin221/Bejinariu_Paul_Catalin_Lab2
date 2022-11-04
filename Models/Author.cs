88ccf57148575b2be3c4a0aa28077b83a8f27f58
Bejinariu Paul Catalin <bejinariucatalin29@gmail.com>
2022-11-04 20:02:33 +0200

    Lab4

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,11 +1,16 @@
-﻿namespace Bejinariu_Paul_Catalin_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Bejinariu_Paul_Catalin_Lab2.Models
 {
 	public class Author
 	{
 		public int ID { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
-		public string FullName { get { return FirstName + " " + LastName; } }
+
+        [Display(Name = "Full Name")]
+        public string FullName { get { return FirstName + " " + LastName; } }
 		public ICollection<Book>? Books { get; set; } // navigation property
 
 	}
---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Bejinariu_Paul_Catalin_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+
+}
